8e7b503c2d0298d3f16ab1b3ed1ef1c808071991
19sky <19sky@LAPTOP-DEBVQI68>
2022-12-21 17:21:42 -0500

    Export frame linked up.

--- Capstone/src/capstoneMainFrame/CapstoneMainFrame.java
+++ Capstone/src/capstoneMainFrame/CapstoneMainFrame.java
@@ -14,8 +14,9 @@ import java.awt.Insets;
 
 public class CapstoneMainFrame {
 
-	private JFrame frame;
-
+	private JFrame mainFrame;
+	private ExportFrame export = new ExportFrame();
+	
 	/**
 	 * Launch the application.
 	 */
@@ -24,7 +25,7 @@ public class CapstoneMainFrame {
 			public void run() {
 				try {
 					CapstoneMainFrame window = new CapstoneMainFrame();
-					window.frame.setVisible(true);
+					window.mainFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -43,19 +44,21 @@ public class CapstoneMainFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame = new JFrame();
+		mainFrame.setBounds(100, 100, 450, 300);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{25, 236, 25, 0};
 		gridBagLayout.rowHeights = new int[]{41, 39, 20, 20, 20, 20, 20, 20, 0, 0};
 		gridBagLayout.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		frame.getContentPane().setLayout(gridBagLayout);
+		mainFrame.getContentPane().setLayout(gridBagLayout);
 		
 		JButton btnExport = new JButton("Export");
 		btnExport.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				mainFrame.dispose();
+				export.setVisible(true);
 			}
 		});
 		
@@ -66,13 +69,13 @@ public class CapstoneMainFrame {
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel.gridx = 1;
 		gbc_lblNewLabel.gridy = 1;
-		frame.getContentPane().add(lblNewLabel, gbc_lblNewLabel);
+		mainFrame.getContentPane().add(lblNewLabel, gbc_lblNewLabel);
 		GridBagConstraints gbc_btnExport = new GridBagConstraints();
-		gbc_btnExport.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnExport.fill = GridBagConstraints.BOTH;
 		gbc_btnExport.insets = new Insets(0, 0, 5, 5);
 		gbc_btnExport.gridx = 1;
 		gbc_btnExport.gridy = 2;
-		frame.getContentPane().add(btnExport, gbc_btnExport);
+		mainFrame.getContentPane().add(btnExport, gbc_btnExport);
 		
 		JButton btnOrders = new JButton("Orders");
 		btnOrders.addActionListener(new ActionListener() {
@@ -80,11 +83,11 @@ public class CapstoneMainFrame {
 			}
 		});
 		GridBagConstraints gbc_btnOrders = new GridBagConstraints();
-		gbc_btnOrders.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnOrders.fill = GridBagConstraints.BOTH;
 		gbc_btnOrders.insets = new Insets(0, 0, 5, 5);
 		gbc_btnOrders.gridx = 1;
 		gbc_btnOrders.gridy = 3;
-		frame.getContentPane().add(btnOrders, gbc_btnOrders);
+		mainFrame.getContentPane().add(btnOrders, gbc_btnOrders);
 		
 		JButton btnCustomer = new JButton("Customers");
 		btnCustomer.addActionListener(new ActionListener() {
@@ -104,23 +107,23 @@ public class CapstoneMainFrame {
 			}
 		});
 		GridBagConstraints gbc_btnInventory = new GridBagConstraints();
-		gbc_btnInventory.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnInventory.fill = GridBagConstraints.BOTH;
 		gbc_btnInventory.insets = new Insets(0, 0, 5, 5);
 		gbc_btnInventory.gridx = 1;
 		gbc_btnInventory.gridy = 4;
-		frame.getContentPane().add(btnInventory, gbc_btnInventory);
+		mainFrame.getContentPane().add(btnInventory, gbc_btnInventory);
 		GridBagConstraints gbc_btnSpreader = new GridBagConstraints();
-		gbc_btnSpreader.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnSpreader.fill = GridBagConstraints.BOTH;
 		gbc_btnSpreader.insets = new Insets(0, 0, 5, 5);
 		gbc_btnSpreader.gridx = 1;
 		gbc_btnSpreader.gridy = 5;
-		frame.getContentPane().add(btnSpreader, gbc_btnSpreader);
+		mainFrame.getContentPane().add(btnSpreader, gbc_btnSpreader);
 		GridBagConstraints gbc_btnCustomer = new GridBagConstraints();
-		gbc_btnCustomer.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnCustomer.fill = GridBagConstraints.BOTH;
 		gbc_btnCustomer.insets = new Insets(0, 0, 5, 5);
 		gbc_btnCustomer.gridx = 1;
 		gbc_btnCustomer.gridy = 6;
-		frame.getContentPane().add(btnCustomer, gbc_btnCustomer);
+		mainFrame.getContentPane().add(btnCustomer, gbc_btnCustomer);
 		
 		JButton btnEmployee = new JButton("Employees");
 		btnEmployee.addActionListener(new ActionListener() {
@@ -129,9 +132,9 @@ public class CapstoneMainFrame {
 		});
 		GridBagConstraints gbc_btnEmployee = new GridBagConstraints();
 		gbc_btnEmployee.insets = new Insets(0, 0, 5, 5);
-		gbc_btnEmployee.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnEmployee.fill = GridBagConstraints.BOTH;
 		gbc_btnEmployee.gridx = 1;
 		gbc_btnEmployee.gridy = 7;
-		frame.getContentPane().add(btnEmployee, gbc_btnEmployee);
+		mainFrame.getContentPane().add(btnEmployee, gbc_btnEmployee);
 	}
 }
---
+++ Capstone/src/capstoneMainFrame/ExportFrame.java
@@ -0,0 +1,51 @@
+package capstoneMainFrame;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.Box;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JSplitPane;
+import javax.swing.JScrollPane;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import javax.swing.ScrollPaneConstants;
+
+public class ExportFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ExportFrame frame = new ExportFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ExportFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+}
