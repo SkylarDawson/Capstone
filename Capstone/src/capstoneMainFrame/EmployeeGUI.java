70303353f0197c50d13939ef7aecc81ba44f01c5
tspri <tspri@192.168.56.1>
2023-01-26 16:18:46 -0500

    Connects to db and lists out general employee info.

--- Capstone/src/capstoneMainFrame/EmployeeGUI.java
+++ Capstone/src/capstoneMainFrame/EmployeeGUI.java
@@ -3,6 +3,10 @@
  */
 package capstoneMainFrame;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 /**
  * @author 19sky
  *
@@ -14,6 +18,28 @@ public class EmployeeGUI {
 	 */
 	public EmployeeGUI() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		String url = "";
+		String username = "";
+		String password = "";
+		
+		try (Connection conn = DriverManager.getConnection(url, username, password)){
+			String query = "SELECT * FROM employees";
+			Statement stmt = conn.createStatement();
+			ResultSet resultSet = stmt.executeQuery(query);
+			
+			
+			while (resultSet.next()) {
+				int id = resultSet.getInt("id");
+				String name = resultSet.getString("name");
+				String position = resultSet.getString("position");
+				System.out.println("ID: " + id + ", Name: " + name + ", Position: " + position);
+			}
+		} catch (SQLException exception) {
+			System.out.println(exception.getMessage());
+		}
 	}
 
 }
