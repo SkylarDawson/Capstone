6a95013be629914435e2b1dcdf8dd6f12ef97c77
tspri <tspri@DESKTOP-JLLTG2L>
2023-01-29 16:27:32 -0500

    Created generic variables and functions for creating an employee

--- Capstone/src/capstoneMainFrame/CreateEmployee.java
+++ Capstone/src/capstoneMainFrame/CreateEmployee.java
@@ -8,12 +8,41 @@ package capstoneMainFrame;
  *
  */
 public class CreateEmployee {
+	
+	private String name;
+	private String department;
+	private int age;
+	private double salary;
+	
 
 	/**
 	 * 
 	 */
-	public CreateEmployee() {
+	public CreateEmployee(String name, String department, int age, double salary) {
 		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.department = department;
+		this.age = age;
+		this.salary = salary;
 	}
+	
+	// Getter functions to obtain general employee information
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
 
 }
