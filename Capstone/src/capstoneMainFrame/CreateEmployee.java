f84c6a593c6ced3ba86b95cffc444d5824561e4a
tspri <tspri@DESKTOP-JLLTG2L>
2023-01-30 16:27:17 -0500

    added ability to delete an employee, and lookup an employee by name/num.

--- Capstone/src/capstoneMainFrame/CreateEmployee.java
+++ Capstone/src/capstoneMainFrame/CreateEmployee.java
@@ -3,6 +3,9 @@
  */
 package capstoneMainFrame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author 19sky
  *
@@ -10,6 +13,7 @@ package capstoneMainFrame;
 public class CreateEmployee {
 	
 	private String name;
+	private int num;
 	private String department;
 	private int age;
 	private double salary;
@@ -18,7 +22,7 @@ public class CreateEmployee {
 	/**
 	 * 
 	 */
-	public CreateEmployee(String name, String department, int age, double salary) {
+	public CreateEmployee(String name, int num, String department, int age, double salary) {
 		// TODO Auto-generated constructor stub
 		this.name = name;
 		this.department = department;
@@ -26,10 +30,24 @@ public class CreateEmployee {
 		this.salary = salary;
 	}
 	
+	// function to have the ability to delete an employee
+	
+	public void deleteEmployee() {
+		this.name = null;
+		this.num = 0;
+		this.department = null;
+		this.age = 0;
+		this.salary = 0.0;
+	}
+	
 	// Getter functions to obtain general employee information
 	
 	public String getName() {
 		return name;
+	}
+	
+	public int getNum() {
+		return num;
 	}
 	
 	public String getDepartment() {
@@ -44,5 +62,38 @@ public class CreateEmployee {
 		return salary;
 	}
 	
+	
+	// List allows for ability to lookup a specific employee
+	
+	public class EmployeeDirectory {
+		private static List<CreateEmployee> employees = new ArrayList<>();
+		
+		public static void addEmployee(CreateEmployee employee) {
+			employees.add(employee);
+		}
+		
+		// search by employee name
+		
+		public static CreateEmployee searchEmployeeName(String name) {
+			for (CreateEmployee employee : employees) {
+				if (employee.getName().equals(name)) {
+					return employee;
+				}
+			}
+			return null;
+		}
+		
+		// search by employee number
+		
+		public static CreateEmployee searchEmployeeNum(int num) {
+			for (CreateEmployee employee : employees) {
+				if (employee.getNum() == num) {
+					return employee;
+				}
+			}
+			return null;
+		}
+		
+	}
 
 }
