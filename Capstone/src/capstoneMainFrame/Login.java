d24b8618822d5f57fe4130332a5f177c74977956
tspri <tspri@DESKTOP-JLLTG2L>
2023-02-12 17:42:54 -0500

    Basic login window created with user id and password.

---
+++ Capstone/src/capstoneMainFrame/Login.java
@@ -0,0 +1,78 @@
+package capstoneMainFrame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Login implements ActionListener {
+	
+	private static JLabel userLabel;
+	private static JTextField userText;
+	private static JLabel passwordLabel;
+	private static JPasswordField passwordText;
+	private static JButton button;
+	private static JLabel success;
+	
+	public static void main(String[] args) {
+		
+		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+		frame.setSize(350, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.add(panel);
+		
+		panel.setLayout(null);
+		
+		
+		userLabel = new JLabel("User ID: ");
+		userLabel.setBounds(10, 20, 80, 25);
+		panel.add(userLabel);
+		
+		userText = new JTextField(20);
+		userText.setBounds(100, 20, 165, 25);
+		panel.add(userText);
+		
+		passwordLabel = new JLabel("Password: ");
+		passwordLabel.setBounds(10, 50, 80, 25);
+		panel.add(passwordLabel);
+		
+		passwordText = new JPasswordField();
+		passwordText.setBounds(100, 50, 165, 25);
+		panel.add(passwordText);
+		
+		button = new JButton("Login");
+		button.setBounds(10, 80, 80, 25);
+		button.addActionListener(new Login());
+		panel.add(button);
+		
+		success = new JLabel("");
+		success.setBounds(10, 110, 300, 25);
+		panel.add(success);
+		
+		frame.setVisible(true);
+		
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String user = userText.getText();
+		String password = passwordText.getText();
+		System.out.println(user + ", " + password);
+		
+		if(user.equals("Jake") && password.equals("password")) {
+			success.setText("Login successful");
+		}
+		else {
+			success.setText("Incorrect user id / password entered.");
+		}
+	}
+
+}
