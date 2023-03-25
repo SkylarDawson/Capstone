fd266aef8b9fae443ad6bee0c84771caec5807d8
19sky <19sky@LAPTOP-DEBVQI68.lan>
2023-03-25 11:06:13 -0400

    Bug Fix

--- Capstone/src/capstoneMainFrame/Spreader.java
+++ Capstone/src/capstoneMainFrame/Spreader.java
@@ -163,7 +163,7 @@ public class Spreader {
 	 */
 	public void claimSpreader(int number, int customerID) throws Exception {
 		
-		String checkSpreader = String.format( "Select COUNT(spreaderNum = \"%x\") from spreaders", number);
+		String checkSpreader = String.format( "Select customerID from spreaders where spreaderNum = \"%x\" ;", number);
 		// Update
 		String updateSpreader = "Update spreaders "
 				+ "SET customerID = ? , "
@@ -172,11 +172,10 @@ public class Spreader {
 						
 		try {
 		    Connection conn = connect();  
-		    
-		    
+		   
 		    Statement stmt  = conn.createStatement();  
             ResultSet rs    = stmt.executeQuery(checkSpreader); 
-            if(rs.getRow() == 0) throw new Exception("Not Valid Spreader\n");
+            if(!rs.next()) throw new Exception("Not Valid Spreader\n");
 		    
 		    PreparedStatement pstmt  = conn.prepareStatement(updateSpreader);  
 		    pstmt.setInt(1, customerID);
